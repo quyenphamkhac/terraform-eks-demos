7d287c61325d506c07950402f492a02d35ad4ceb
Quyen Pham Khac <phamkhacquyen1996@gmail.com>
2022-11-15 17:03:54 +0700

    docs: update folder name

--- 02-tf-blocks/tf-providers-resources/main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "myec2vm" {
-  ami           = "ami-094bbd9e922dc515d"
-  instance_type = "t3.micro"
-  user_data     = file("${path.module}/app1-install.sh")
-  tags = {
-    "Name" = "EC2 Demo"
-  }
-}
